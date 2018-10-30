b72c16285ae11814522db72a317d24df8995af4d
kuuhaku <juanpablolvl99@gmail.com>
2018-10-30 16:13:44 -0300

    adding component exercise

---
+++ exercises/Exercise 6/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <headers></headers>
+        <hr>
+        <div class="row">
+            <servers></servers>
+        </div>
+        <details></details>
+        <hr>
+        <footers></footers>
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue';
+    import Server from './Server.vue';
+    import Detail from './Detail.vue';
+    import Footer from './Footer.vue';
+
+    export default {
+        components: {
+            'headers': Header,
+            'servers': Server,
+            'details': Detail,
+            'footers': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
