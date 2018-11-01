282356a2a0d4ec29711558d1c16b80478987791f
kuuhaku <juanpablolvl99@gmail.com>
2018-11-01 00:07:58 -0300

    adding communication between components

---
+++ lectures/section_8/src/components/UserDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{myName}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['myName']
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
