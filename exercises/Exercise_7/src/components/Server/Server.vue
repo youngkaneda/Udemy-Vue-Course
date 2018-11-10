9b084b1c8e3e802b2f8ad429b254f7715883687b
kuuhaku <juanpablolvl99@gmail.com>
2018-11-09 21:19:59 -0300

    updatin and resolving exercise

---
+++ exercises/Exercise_7/src/components/Server/Server.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="list-group-item"
+        @click="serverSelected">
+        Server {{appServer.id}}
+    </li>
+</template>
+
+<script>
+    import {bus} from '../../main'
+    export default {
+        props: ['appServer'],
+        methods: {
+            serverSelected: function() {
+                bus.$emit("serverSelected", this.appServer);
+            }
+        },
+        created: function() {
+            bus.$on("repair", data => {
+                this.appServer.status = data;
+            });
+        }
+    }
+</script>
+
+<style>
+</style>
--- exercises/Exercise_7/src/components/Server/ServerDetails.vue
+++ exercises/Exercise_7/src/components/Server/ServerDetails.vue
@@ -1,11 +1,33 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <div v-if="server">
+            <p >Server {{server.id}} are currently selected, Status: {{server.status}}</p>
+            <br>
+            <button @click="repairStatus">Repair status</button>
+        </div>
+        <p v-else>Select a server</p>
     </div>
-
 </template>
 
 <script>
+    import {bus} from '../../main'
+    export default {
+        data: function() {
+            return {
+                server: null
+            }
+        },
+        methods: {
+            repairStatus: function() {
+                bus.$emit("repair", "Normal");
+            }
+        },
+        created: function() {
+            bus.$on("serverSelected", server => {
+                this.server = server;
+            })
+        }
+    }
 </script>
 
 <style>
