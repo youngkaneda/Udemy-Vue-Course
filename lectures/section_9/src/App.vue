52b4a90639b9e6d3f5f5253ad4c635e5c42a80b1
kuuhaku <juanpablolvl99@gmail.com>
2018-11-10 21:26:59 -0300

    adding a basic example of slots

---
+++ lectures/section_9/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <quote>
+                   <p>A wonderful quote!</p>
+               </quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+    export default {
+        components: {
+            quote: Quote
+        }
+    }
+</script>
+
+<style>
+</style>
