a4b44450920adb5b7248e1491f8053b6d2967f87
kuuhaku <juanpablolvl99@gmail.com>
2018-11-15 16:41:02 -0300

    adding code lecture and exercise about custom directives

---
+++ exercises/Exercise_9/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-one:click="clicked">Test</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            one: {
+                bind(el, binding, vnode) {
+                    el.addEventListener(binding.arg, binding.value);
+                }
+            }
+        },
+        methods: {
+            clicked: function() {
+                alert('clicked');
+            }
+        }
+    }
+</script>
+
+<style></style>
