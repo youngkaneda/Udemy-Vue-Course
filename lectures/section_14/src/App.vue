f15d74a9dfa681e7f72c00d9aca92768bacf6150
kuuhaku <juanpablolvl99@gmail.com>
2018-11-20 20:12:02 -0300

    adding some code of section about animations

---
+++ lectures/section_14/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <br>
+                <button class="btn btn-primary" @click="show = !show">show alert</button>
+                <br><br>
+                <transition name="fade">
+                    <div class="alert alert-info" v-if="show">some alert info</div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: false
+            }
+        }
+    }
+</script>
+
+<style>
+    .fade-enter {
+        opacity: 0;
+    }
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+    .fade-leave {
+        /* opacity: 1; */
+    }
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+</style>
