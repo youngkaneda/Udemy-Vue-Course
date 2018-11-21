c6d674442c20d82fe8154bd08b2aa0f4becc476f
kuuhaku <juanpablolvl99@gmail.com>
2018-11-21 19:32:28 -0300

    adding more vue trasitions features

--- lectures/section_14/src/App.vue
+++ lectures/section_14/src/App.vue
@@ -6,8 +6,19 @@
                 <br>
                 <button class="btn btn-primary" @click="show = !show">show alert</button>
                 <br><br>
-                <transition name="fade">
+                <transition name="slide" type="animation" appear>
                     <div class="alert alert-info" v-if="show">some alert info</div>
+                </transition>
+                <br>
+                <transition 
+                    enter-active-class="animated bounce"
+                    leave-active-class="animated shake">
+                    <div class="alert alert-info" v-if="show">some alert info</div>
+                </transition>
+                <br>
+                <transition name="slide" type="animation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">some alert info</div>
+                    <div class="alert alert-warning" v-else key="warn">some alert warn</div>
                 </transition>
             </div>
         </div>
@@ -25,17 +36,36 @@
 </script>
 
 <style>
-    .fade-enter {
+    .slide-enter {
         opacity: 0;
     }
-    .fade-enter-active {
+    .slide-enter-active {
+        /* remember to set who dictates the animation length */
         transition: opacity 1s;
+        animation: slide-in 1s ease-in forwards;
     }
-    .fade-leave {
-        /* opacity: 1; */
-    }
-    .fade-leave-active {
+    /* .slide-leave {
+    } */
+    .slide-leave-active {
+        /* remember to set who dictates the animation length */
+        animation: slide-out 1s ease-in forwards;
         transition: opacity 1s;
         opacity: 0;
+    }
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0px);
+        }
+    }
+    @keyframes slide-out {
+        from {
+            transform: translateY(0px);
+        }
+        to {
+            transform: translateY(20px);            
+        }
     }
 </style>
