3516b1319eb320abef9714a7b4e8aa522f6990e0
kuuhaku <juanpablolvl99@gmail.com>
2018-12-03 21:54:01 -0300

    adding feature project structure, and mapping the routes

---
+++ exercises/Project_Final/src/components/stocks/Stocks.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <app-stock :v-for="stock in stocks"></app-stock>
+    </div>
+</template>
+
+<script>
+    import Stock from './Stock.vue';
+
+    export default {
+        data() {
+            return {
+                stocks: [
+                    {id: 1, name: 'BMW', price: '100'},
+                ],
+            };
+        },
+        components: {
+            appStock: Stock,
+        },
+    };
+</script>
+
+<style>
+
+</style>
